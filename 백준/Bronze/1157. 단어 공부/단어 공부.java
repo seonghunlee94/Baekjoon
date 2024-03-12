d7c38a05e1f7f13d100df742c2e9692d928a609d
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-03-12 23:23:05 +0900

    [Bronze I] Title: 단어 공부, Time: 464 ms, Memory: 29008 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] getAlphabetCount(String str) {
+        int[] count = new int[26];
+        for (int i=0; i<str.length(); i++) {
+            count[str.charAt(i) - 'A']++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+
+        int[] count = getAlphabetCount(str);
+
+        int maxCount = -1;
+        char maxAlphabet = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                maxAlphabet = (char)('A' + i);
+            }
+            else if (count[i] == maxCount) {
+                maxAlphabet = '?';
+            }
+        }
+
+        System.out.println(maxAlphabet);
+
+    }
+
+}
