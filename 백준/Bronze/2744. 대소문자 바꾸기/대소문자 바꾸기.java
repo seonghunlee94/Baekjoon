04d10964cec7f1a60451cf692e2d55bb7fb2cc90
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-01-21 23:20:12 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 92 ms, Memory: 12772 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        char[] ch = sc.next().toCharArray();
+        char[] changCh = ch ;
 
-        String str = sc.next();
-
-        for (int i = 0; i < str.length(); i++) {
-
-            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z' ) {
-                System.out.print((char)('a' + str.charAt(i) - 'A'));
-            } else {
-                System.out.print((char)('A' + str.charAt(i) - 'a'));
+        for (int i =0; i < ch.length; i++) {
+            if (ch[i] >= 65 && ch[i] < 91) {
+                changCh[i] = (char)(ch[i] + 32);
             }
+
+            else if (ch[i] >= 97 && ch[i] < 122) {
+                changCh[i] = (char)(ch[i] - 32);
+            }
+
+            System.out.print(changCh[i]);
         }
 
     }
