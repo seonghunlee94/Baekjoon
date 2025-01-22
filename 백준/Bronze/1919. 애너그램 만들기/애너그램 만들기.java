5716450cef0eda147da74fcae32874d5e2b5011d
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-01-22 22:36:01 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 96 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,30 +1,35 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String a = sc.next();
-        String b = sc.next();
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.next();
+        String word2 = sc.next();
 
         int[] countA = new int[26];
         int[] countB = new int[26];
 
-        for (int i = 0; i < a.length(); i++)
-            countA[a.charAt(i) - 'a']++;
-        for (int i = 0; i < b.length(); i++)
-            countB[b.charAt(i) - 'a']++;
+        word1 = word1.toUpperCase();
+        word2 = word2.toUpperCase();
 
-        int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (countA[i] > countB[i])
-                ans += countA[i] - countB[i];
-            else if(countA[i] < countB[i])
-                ans += countB[i] - countA[i];
-
+        for (int i = 0; i < word1.length(); i++) {
+            int x = word1.charAt(i) - 65;
+            countA[x] += 1;
         }
 
-        System.out.println(ans);
+        for (int j = 0; j < word2.length(); j++) {
+            int y = word2.charAt(j) - 65;
+            countB[y] += 1;
+        }
+
+        int num = 0;
+        for (int n = 0; n < 26; n++) {
+            num += Math.abs(countA[n] - countB[n]) ;
+        }
+
+        System.out.println(num);
 
     }
 }
