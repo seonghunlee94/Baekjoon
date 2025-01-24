c14e0da86a6f6d5379011f9ef3510f24d7f7484e
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-01-24 22:31:23 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 12900 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        String doc = sc.nextLine();
+        String text = sc.nextLine();
         String word = sc.nextLine();
 
-        int count = 0;
-        int startIndex = 0;
-        while (true) {
-            int findIndex = doc.indexOf(word, startIndex);
+        int textInCount = 0;
 
-            if(findIndex < 0)
-                break;
 
-            count++;
-            startIndex = findIndex + word.length();
+        for (int i = 0; i <= text.length() - word.length(); i++) {
+
+            for (int j=0; j < word.length(); j++) {
+
+                if (text.charAt(i+j) != word.charAt(j)) {
+                    break;
+                }
+
+                if (j == word.length()-1) {
+                    textInCount++;
+                    i = i + j;
+                }
+            }
         }
-        System.out.println(count);
+
+        System.out.println(textInCount);
 
     }
 
