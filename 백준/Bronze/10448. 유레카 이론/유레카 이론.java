fb0d9c0d281f8c8d9dfba3bae0462780bb3b062d
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-02-04 21:34:36 +0900

    [Bronze I] Title: 유레카 이론, Time: 172 ms, Memory: 16880 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -3,62 +3,65 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    static boolean[] sumTwoTriangle = new boolean[1001];
+    static boolean[] eurekaNumber = new boolean[1001];
+
+    public static void sumTowTriangle (int[] triangleList) {
+
+        int num = 0;
+        for (int i = 1; i < triangleList.length; i++) {
+            for (int j = 1; j < triangleList.length; j++) {
+                num = triangleList[i] + triangleList[j];
+                if (num < 1000) sumTwoTriangle[num] = true;
+            }
+        }
+
+    }
+
+    public static void checkEureka (int[] triangleList) {
+
+        int num = 0;
+
+        for (int i = 1; i < sumTwoTriangle.length; i++) {
+            if (sumTwoTriangle[i] == true)
+                for (int j = 1; j < triangleList.length; j++) {
+                    num = i + triangleList[j];
+                    if (num < 1001) eurekaNumber[num] = true;
+                }
+        }
+
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int triangularNum = 0;
-        int[] triangularList = new int[44];
-
+        // 삼각수 리스트 구하기.
+        int[] triangleList = new int[45];
         for (int i = 1; i < 45; i++) {
-
-            triangularNum += i;
-            triangularList[i-1] = triangularNum;
-
+            int num = i * (i + 1) / 2;
+            if (num < 1001) triangleList[i] = num;
         }
 
-//        System.out.println(Arrays.toString(triangularList));
+        // 세 개의 삼감수의 합이 1000 이하인 경우.
+        // 1. 두 삼각수의 합으로 1~999까지의 나올 수 있는 수 구하기.
+        sumTowTriangle(triangleList);
+
+        // 2. 합으로 나온 값에 삼각수 값을 더해 유레카 찾기.
+        checkEureka(triangleList);
 
         int T = sc.nextInt();
-        boolean[] checked = new boolean[T];
-        int start = 0;
-        int end = 43;
-        
-        while (T > 0) {
 
-            int num = sc.nextInt();
+        while (T-- > 0) {
 
-            for (int i=0; i < 43; i++) {
+            int K = sc.nextInt();
 
-//                System.out.println("-----------------------");
-
-                while (start <= end) {
-//                    System.out.println(triangularList[end]);
-
-                    if (num == triangularList[i] + triangularList[start] + triangularList[end]) {
-                        checked[T-1] = true;
-                        break;
-                    } else if (num > triangularList[i] + triangularList[start] + triangularList[end]) {
-                        start++;
-                    } else {
-                        end--;
-                    }
-                }
-                start = 0;
-                end = 43;
-
-                if (checked[T-1] == true) {
-                    System.out.println(1);
-//                    System.out.println("-----------------------");
-                    break;
-                }
-
-            }
-
-            if (checked[T-1] == false) {
+            if (eurekaNumber[K] == true) {
+                System.out.println(1);
+            } else {
                 System.out.println(0);
             }
-            T--;
 
         }
 
