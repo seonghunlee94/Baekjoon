731024e796a7694dde4512c011437926c60b05c4
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-02-11 23:30:54 +0900

    [Silver V] Title: 나이순 정렬, Time: 2108 ms, Memory: 148180 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        Member[] member = new Member[N];
+
+        for (int i = 0; i < N; i++) {
+            member[i] = new Member(sc.nextInt(), sc.next(), i);
+        }
+
+        Arrays.sort(member, new Comparator<Member>() {
+            @Override
+            public int compare(Member o1, Member o2) {
+                if (o1.age == o2.age)
+                    o1.name.compareTo(o2.name);
+                return o1.age - o2.age;
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(member[i].age + " " + member[i].name);
+        }
+
+    }
+
+}
+
+
+class Member {
+    int age;
+    String name;
+    int idx;
+
+    public Member (int age, String name, int idx) {
+        this.age = age;
+        this.name = name;
+        this.idx = idx;
+    }
+}
