ac26aa3c73b76136f9c21ae52e815afe86b25094
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-03-19 21:49:43 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 92 ms, Memory: 12812 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,35 +1,29 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         String word1 = sc.next();
         String word2 = sc.next();
 
-        int[] countA = new int[26];
-        int[] countB = new int[26];
+        int[] alpabetCount1 = new int[26];
+        int[] alpabetCount2 = new int[26];
 
-        word1 = word1.toUpperCase();
-        word2 = word2.toUpperCase();
+        int delWord = 0;
 
         for (int i = 0; i < word1.length(); i++) {
-            int x = word1.charAt(i) - 65;
-            countA[x] += 1;
+            alpabetCount1[word1.charAt(i) - 97]++;
+        }
+        for (int i = 0; i < word2.length(); i++) {
+            alpabetCount2[word2.charAt(i) - 97]++;
         }
 
-        for (int j = 0; j < word2.length(); j++) {
-            int y = word2.charAt(j) - 65;
-            countB[y] += 1;
+        for (int i = 0; i < 26; i++) {
+            delWord += Math.abs(alpabetCount1[i] - alpabetCount2[i]);
         }
 
-        int num = 0;
-        for (int n = 0; n < 26; n++) {
-            num += Math.abs(countA[n] - countB[n]) ;
-        }
-
-        System.out.println(num);
-
+        System.out.println(delWord);
     }
 }
