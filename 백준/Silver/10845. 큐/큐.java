604610b3c4d36bc43729e7857eca79ae3278d065
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-04-07 16:32:51 +0900

    [Silver IV] Title: 큐, Time: 136 ms, Memory: 18764 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,6 +1,6 @@
-
 import java.io.*;
-import java.util.NoSuchElementException;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,89 +8,33 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        MyQueue<Integer> q = new MyQueue<>();
+        Deque<Integer> dq = new LinkedList<>();
 
         while (N-- > 0) {
             String[] cmd = br.readLine().split(" ");
 
             switch (cmd[0]) {
                 case "push":
-                    q.enqueue(Integer.parseInt(cmd[1].trim()));
+                    dq.offerLast(Integer.parseInt(cmd[1]));  // 뒤에 넣기
                     break;
                 case "pop":
-                    bw.write(q.isEmpty() ? "-1\n" : q.dequeue() + "\n");
+                    bw.write(dq.isEmpty() ? "-1\n" : dq.pollFirst() + "\n"); // 앞에서 꺼내기
                     break;
                 case "size":
-                    bw.write(q.size() + "\n");
+                    bw.write(dq.size() + "\n");
                     break;
                 case "empty":
-                    bw.write(q.isEmpty() ? "1\n" : "0\n");
+                    bw.write(dq.isEmpty() ? "1\n" : "0\n");
                     break;
                 case "front":
-                    bw.write(q.isEmpty() ? "-1\n" : q.getFront() + "\n");
+                    bw.write(dq.isEmpty() ? "-1\n" : dq.peekFirst() + "\n"); // 앞 값 확인
                     break;
                 case "back":
-                    bw.write(q.isEmpty() ? "-1\n" : q.getRear() + "\n");
+                    bw.write(dq.isEmpty() ? "-1\n" : dq.peekLast() + "\n");  // 뒤 값 확인
                     break;
             }
         }
 
         bw.flush();
     }
-}
-
-
-class MyQueue<E> {
-
-    private int size = 0;
-    private Node<E> frontNode = null;
-    private Node<E> rearNode = null;
-
-    public static class Node<E> {
-        E item;
-        Node<E> next;
-        Node(E element, Node<E> next) {
-            this.item = element;
-            this.next = next;
-        }
-
-    }
-
-    public void enqueue(E element) {
-        Node<E> newNode = new Node<>(element, null);
-
-        if (size == 0) frontNode = newNode;
-        else rearNode.next = newNode;
-        rearNode = newNode;
-        size++;
-    }
-
-    public E dequeue() {
-        if (size == 0)
-            throw new NoSuchElementException("Queue is empty.");
-
-        E item = frontNode.item;
-        frontNode = frontNode.next;
-        if (frontNode == null)
-            rearNode = null;
-        size--;
-        return item;
-    }
-
-    public E getFront() {
-        return frontNode.item;
-    }
-
-    public E getRear() {
-        return rearNode.item;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
 }
