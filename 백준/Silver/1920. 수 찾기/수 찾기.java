0f3b68d86db120f7d97d333409ac772c561d039c
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2025-04-20 23:15:53 +0900

    [Silver IV] Title: 수 찾기, Time: 2156 ms, Memory: 208492 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,6 +2,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static boolean isExist(int[] arr, int x) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (arr[m] < x) l = m + 1;
+            else if (arr[m] > x) r = m - 1;
+            else return true;
+        }
+        return false;
+    }
 
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,12 +31,11 @@ public class Main {
             int x = sc.nextInt();
 
             // 2. arr에 x가 있는지 확인한다.
-            if (Arrays.binarySearch(arr, x) >= 0) // 값이 있는 경우 인덱스 리턴
+            if (isExist(arr, x))
                 System.out.println(1);
             else {
                 System.out.println(0);
             }
-
 
         }
 
